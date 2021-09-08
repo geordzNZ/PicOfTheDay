5863918f5cda453257e13ec3b76bbda36dc86021
Geordie King <geordie.king@gmail.com>
2021-09-08 23:07:50 +0100

    fix js file

--- JS/index.js
+++ JS/index.js
@@ -2,16 +2,17 @@ const providers = [
   { name: 'NASA', url: ''},
   { name: 'Animals', url: ''},
   { name: 'Scenery', url: ''}
-}
+]
 
 //Button Listeners and HTMl elements
   document.querySelector('#btnGetPic').addEventListener('click', getPic)
 
-  let source = document.getElementById('cmboSources').value
+  let source = document.getElementById('cmboSources')
 
-
+console.log("Hello from the JS file")
 function getPic(){
-  document.getElementById('tmpAnswer').innerText =   // connect to URL
+  document.getElementById('tmpAnswer').innerText =  source.value 
+  // connect to URL
   // get image
   // return to page
 }

14183b08f8ff3c8d26473a0523eef07f6865a087
Geordie King <geordie.king@gmail.com>
2021-09-08 23:02:28 +0100

    set up js file, with providers and structure

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+const providers = [
+  { name: 'NASA', url: ''},
+  { name: 'Animals', url: ''},
+  { name: 'Scenery', url: ''}
+}
+
+//Button Listeners
+  document.querySelector('#btnGetPic').addEventListener('click', getPic)
+
+
+
+function getPic(){
+  // connect to URL
+  // get image
+  // return to page
+}
